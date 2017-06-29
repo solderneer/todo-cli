8f080b0d76400f6a849d3ab9fa3ce1387f78530d
solderneer <sudhar393@gmail.com>
2017-06-30 07:26:51 +0800

    wow c++ is so fun

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,16 @@ using namespace std;
 
 int main(void)
 {
-    TodoElement* firstElem = new TodoElement("00:00:00", "do my laundy");
-    TodoElement* secondElem = new TodoElement("00:00:01", "do my homework");
+    TodoElement firstElem = TodoElement("00:00:00", "do my laundy");
+    TodoElement secondElem = TodoElement("00:00:01", "do my homework");
+    TodoElement thirdElem = TodoElement("00:00:01", "do my work");
 
-    cout << "The first todo is to " << firstElem->getTodoText() << " at " << firstElem->getTimeStamp() << endl;
-    cout << "The next todo element is at address " << firstElem->getNextElement() << endl;
 
-    TodoElement *nextptr = firstElem->getNextElement();
+    cout << "The first todo is to " << firstElem.getTodoText() << " at " << firstElem.getTimeStamp() << endl;
+    cout << "The next todo element is at address " << firstElem.getNextElement() << endl;
 
-    cout << "The second todo is to " << secondElem->getTodoText() << " at " << secondElem->getTimeStamp() << endl;
-    cout << "The next todo element is at address " << firstElem->getNextElement() << endl;
+    TodoElement *nextptr = firstElem.getNextElement();
+
+    cout << "The second todo is to " << secondElem.getTodoText() << " at " << secondElem.getTimeStamp() << endl;
+    cout << "The next todo element is at address " << secondElem.getNextElement() << endl;
 }
