c7b6be14f94c65062694bff10ae9410770410dd9
solderneer <sudhar393@gmail.com>
2017-08-09 16:33:40 +0800

    got basic ncurses exaple working

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
+/*
 
-int main(void)
-{
-    std::cout << "Welcome to the todolist app" << std::endl;
-    std::cout << "Checking for the .todolist list" << std::endl;
+  CURHELLO.C
+  ==========
+  (c) Copyright Paul Griffiths 1999
+  Email: mail@paulgriffiths.net
+
+  "Hello, world!", ncurses style.
+
+*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>                  /*  for sleep()  */
+#include <curses.h>
+
+
+int main(void) {
+
+    WINDOW * mainwin;
+
+    
+    /*  Initialize ncurses  */
+
+    if ( (mainwin = initscr()) == NULL ) {
+    fprintf(stderr, "Error initialising ncurses.\n");
+    exit(EXIT_FAILURE);
+    }
+
+
+    /*  Display "Hello, world!" in the centre of the
+    screen, call refresh() to show our changes, and
+    sleep() for a few seconds to get the full screen effect  */
+
+    mvaddstr(13, 33, "Hello, world!");
+    refresh();
+    sleep(3);
+
+
+    /*  Clean up after ourselves  */
+
+    delwin(mainwin);
+    endwin();
+    refresh();
+
+    return EXIT_SUCCESS;
 }
